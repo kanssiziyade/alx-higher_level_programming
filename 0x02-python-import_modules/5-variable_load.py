386e855bb86024ef11fa14db950f878d02fda468
kanssiziyade <ziyade577@gmail.com>
2024-03-20 07:20:42 -0700

    alx

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
-f __name__ == "__main__":
-        """Print the value of variable a from variable_load_5."""
-            from variable_load_5 import a
+#!/usr/bin/python3
 
-                print(a)
+if __name__ == "__main__":
+    from variable_load_5 import a
+    print(a)
