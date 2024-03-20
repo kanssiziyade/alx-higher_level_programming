ebcf2c2a5142bbd0a9eeb921d454afb7b3f7ec31
kanssiziyade <ziyade577@gmail.com>
2024-03-20 07:06:26 -0700

    alx

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    # Check if idx is within the range of the list indices
+    if 0 <= idx < len(my_list):
+        # Delete the item at the specified index
+        del my_list[idx]
+
+    # Return the modified list
+    return my_list
